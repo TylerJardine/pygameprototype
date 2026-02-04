1d4c1d2069d59daadde42f8fb6ba63173eca04c2
Corinna MacIntyre <corinnamacintyre@cmail.carleton.ca>
2026-02-04 15:57:02 -0500

    messing w the coin shape

--- plan.py
+++ plan.py
@@ -27,4 +27,4 @@ screen.fill(BLACK)
 
 #create sprite
 player_rect = pygame.Rect(WIDTH // 2, HEIGHT // 1, 30, 30)
-coin_circl = pygame.Circle(WIDTH // 2, HEIGHT // 5, 10, 10)
+coin_circl = pygame.Rect(WIDTH // 2, HEIGHT // 5, 10, 10)
